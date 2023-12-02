d1216b5c8247789d7d05e4a2bcb37c9435cecfe2
TheVSAKeeper <votintcev1@mail.ru>
2023-12-03 02:29:50 +0300

    Refactor DbContextBase and remove unnecessary code
    This commit refactors the DbContextBase class to simplify and optimize it. It removes redundant code and improves effectiveness of SaveChanges methods by isolating SaveCreated and SaveUpdated functions. Additionally, unnecessary code in OnModelCreating method was removed to improve the performance. The changes also include minor formatting alterations for readability and code consistency.

--- Surveys.Infrastructure/Base/DbContextBase.cs
+++ Surveys.Infrastructure/Base/DbContextBase.cs
@@ -7,30 +7,17 @@ using Surveys.Domain.Base;
 
 namespace Surveys.Infrastructure.Base;
 
-public abstract class DbContextBase : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+public abstract class DbContextBase(DbContextOptions options) : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>(options)
 {
     private const string DefaultUserName = "Anonymous";
+    private const string CreatedAt = "CreatedAt";
+    private const string CreatedBy = "CreatedBy";
+    private const string UpdatedAt = "UpdatedAt";
+    private const string UpdatedBy = "UpdatedBy";
 
-    protected DbContextBase(DbContextOptions options) : base(options)
-    {
-        LastSaveChangesResult = new SaveChangesResult();
-    }
+    public SaveChangesResult LastSaveChangesResult { get; } = new();
 
-    public SaveChangesResult LastSaveChangesResult { get; }
-
-    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new())
-    {
-        try
-        {
-            DbSaveChanges();
-            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
-        }
-        catch (Exception exception)
-        {
-            LastSaveChangesResult.Exception = exception;
-            return Task.FromResult(0);
-        }
-    }
+    public override int SaveChanges() => SaveChanges(true);
 
     public override int SaveChanges(bool acceptAllChangesOnSuccess)
     {
@@ -46,26 +33,14 @@ public abstract class DbContextBase : IdentityDbContext<ApplicationUser, Applica
         }
     }
 
-    public override int SaveChanges()
-    {
-        try
-        {
-            DbSaveChanges();
-            return base.SaveChanges();
-        }
-        catch (Exception exception)
-        {
-            LastSaveChangesResult.Exception = exception;
-            return 0;
-        }
-    }
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new()) => SaveChangesAsync(true, cancellationToken);
 
-    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new())
     {
         try
         {
             DbSaveChanges();
-            return base.SaveChangesAsync(cancellationToken);
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
         }
         catch (Exception exception)
         {
@@ -76,69 +51,46 @@ public abstract class DbContextBase : IdentityDbContext<ApplicationUser, Applica
 
     private void DbSaveChanges()
     {
-        const string CreatedAt = "CreatedAt";
-        const string CreatedBy = "CreatedBy";
-        const string UpdatedAt = "UpdatedAt";
-        const string UpdatedBy = "UpdatedBy";
+        SaveCreated();
+        SaveUpdated();
+    }
 
-        IEnumerable<EntityEntry> createdEntries = ChangeTracker.Entries().Where(e => e.State == EntityState.Added);
+    private void SaveCreated()
+    {
+        IEnumerable<EntityEntry> createdEntries = ChangeTracker
+            .Entries()
+            .Where(e => e.State == EntityState.Added);
 
         foreach (EntityEntry entry in createdEntries)
         {
-            if (entry.Entity is not IAuditable)
-                continue;
-
-            DateTime creationDate = DateTime.Now.ToUniversalTime();
-
-            object? userName = entry.Property(CreatedBy).CurrentValue == null
-                ? DefaultUserName
-                : entry.Property(CreatedBy).CurrentValue;
-
-            object? updatedAt = entry.Property(UpdatedAt).CurrentValue;
-            object? createdAt = entry.Property(CreatedAt).CurrentValue;
-
-            if (createdAt != null)
+            if (entry.Entity is IAuditable)
             {
-                if (DateTime.Parse(createdAt.ToString()).Year > 1970)
-                    entry.Property(CreatedAt).CurrentValue = ((DateTime)createdAt).ToUniversalTime();
-                else
-                    entry.Property(CreatedAt).CurrentValue = creationDate;
-            }
-            else
-            {
-                entry.Property(CreatedAt).CurrentValue = creationDate;
-            }
+                object userName = entry.Property(CreatedBy).CurrentValue ?? DefaultUserName;
+                DateTime creationDate = DateTime.Now.ToUniversalTime();
 
-            if (updatedAt != null)
-            {
-                if (DateTime.Parse(updatedAt.ToString()).Year > 1970)
-                    entry.Property(UpdatedAt).CurrentValue = ((DateTime)updatedAt).ToUniversalTime();
-                else
-                    entry.Property(UpdatedAt).CurrentValue = creationDate;
-            }
-            else
-            {
-                entry.Property(UpdatedAt).CurrentValue = creationDate;
-            }
+                entry.Property(CreatedAt).CurrentValue ??= creationDate;
+                entry.Property(UpdatedAt).CurrentValue ??= creationDate;
 
-            entry.Property(CreatedBy).CurrentValue = userName;
-            entry.Property(UpdatedBy).CurrentValue = userName;
+                entry.Property(CreatedBy).CurrentValue = userName;
+                entry.Property(UpdatedBy).CurrentValue = userName;
+            }
 
             LastSaveChangesResult.AddMessage($"ChangeTracker has new entities: {entry.Entity.GetType()}");
         }
+    }
 
-        IEnumerable<EntityEntry> updatedEntries = ChangeTracker.Entries().Where(e => e.State == EntityState.Modified);
+    private void SaveUpdated()
+    {
+        IEnumerable<EntityEntry> updatedEntries = ChangeTracker
+            .Entries()
+            .Where(e => e.State == EntityState.Modified);
 
         foreach (EntityEntry entry in updatedEntries)
         {
             if (entry.Entity is IAuditable)
             {
-                object? userName = entry.Property(UpdatedBy).CurrentValue == null
-                    ? DefaultUserName
-                    : entry.Property(UpdatedBy).CurrentValue;
-
+                entry.Property(UpdatedBy).CurrentValue ??= DefaultUserName;
                 entry.Property(UpdatedAt).CurrentValue = DateTime.Now.ToUniversalTime();
-                entry.Property(UpdatedBy).CurrentValue = userName;
             }
 
             LastSaveChangesResult.AddMessage($"ChangeTracker has modified entities: {entry.Entity.GetType()}");
@@ -148,22 +100,6 @@ public abstract class DbContextBase : IdentityDbContext<ApplicationUser, Applica
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-
-        builder.ApplyConfigurationsFromAssembly(typeof(AppContext).Assembly);
-
-        MethodInfo applyGenericMethod = typeof(ModelBuilder).GetMethods(BindingFlags.Instance | BindingFlags.Public).First(x => x.Name == "ApplyConfiguration");
-
-        foreach (Type type in Assembly.GetExecutingAssembly().GetTypes().Where(c => c.IsClass && !c.IsAbstract && !c.ContainsGenericParameters))
-        {
-            foreach (Type item in type.GetInterfaces())
-            {
-                if (!item.IsConstructedGenericType || item.GetGenericTypeDefinition() != typeof(IEntityTypeConfiguration<>))
-                    continue;
-
-                MethodInfo applyConcreteMethod = applyGenericMethod.MakeGenericMethod(item.GenericTypeArguments[0]);
-                applyConcreteMethod.Invoke(builder, new[] { Activator.CreateInstance(type) });
-                break;
-            }
-        }
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
