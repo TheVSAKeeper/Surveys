064f7f6d684cd16b52ac2ec9a5c22caefa3edcaa
TheVSAKeeper <votintcev1@mail.ru>
2024-01-15 02:41:21 +0300

    Survey editing page layout.

--- Surveys.WPF/Endpoints/SurveysEndpoints/Edit/SurveyEditFormViewModel.cs
+++ Surveys.WPF/Endpoints/SurveysEndpoints/Edit/SurveyEditFormViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 using MediatR;
+using Surveys.Domain;
 using Surveys.WPF.Shared.ViewModels;
 
 namespace Surveys.WPF.Endpoints.SurveysEndpoints.Edit;
@@ -7,6 +9,57 @@ namespace Surveys.WPF.Endpoints.SurveysEndpoints.Edit;
 public class SurveyEditFormViewModel : ViewModelBase
 {
     private SurveyEditDto? _survey;
+
+    public SurveyEditFormViewModel()
+    {
+        Patient patient = new()
+        {
+            LastName = "LastName",
+            FirstName = "FirstName",
+            Patronymic = "Patronymic",
+            Gender = Gender.Male,
+            BirthDate = new DateOnly()
+        };
+
+        IEnumerable<Question> questions = Enumerable.Range(0, 10)
+            .Select(x => new Question
+            {
+                Content = $"Question {x}"
+            });
+
+        AnamnesisTemplate[] anamnesisTemplates = Enumerable.Range(0, 5)
+            .Select(x => new AnamnesisTemplate
+            {
+                Name = $"Template {x}",
+                Questions = new ObservableCollection<Question>(questions)
+            })
+            .ToArray();
+
+        IEnumerable<Anamnesis> anamneses = Enumerable.Range(0, 5)
+            .Select(x => new Anamnesis
+            {
+                AnamnesisTemplate = anamnesisTemplates[x],
+                AnamnesisAnswers = questions.Select(question => new AnamnesisAnswer
+                    {
+                        Question = question,
+                        Answers = new List<Answer> { new() { Content = "Answer" } }
+                    })
+                    .ToList()
+            });
+
+        //CreatedAnamneses = new ObservableCollection<Anamnesis>(anamneses);
+
+        Survey = new SurveyEditDto
+        {
+            Patient = patient,
+            CreatedAt = DateTime.UtcNow,
+            Id = default,
+            Complaint = "null",
+            IsComplete = false,
+            CreatedBy = "null",
+            Anamneses = anamneses.ToList()
+        };
+    }
 
     public SurveyEditFormViewModel(
         IMediator mediator,
