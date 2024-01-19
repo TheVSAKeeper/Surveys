34fbb614a147f656904f680e03f9ff3d65e687d1
Савелий Вотинцев <nissanx1@mail.ru>
2024-01-20 02:00:02 +0300

    Tests of answers variability.

--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -5,6 +5,7 @@ namespace ConsoleTest;
 internal class ApplicationDbContext : DbContext
 {
     public DbSet<Question> Questions { get; set; } = null!;
+    public DbSet<Answer> Answers { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -25,6 +26,7 @@ internal class Program
         using ApplicationDbContext context = InitializeDb();
 
         DbSet<Question> questions = context.Questions;
+        DbSet<Answer> answers = context.Answers;
 
         Console.WriteLine();
         Console.ReadLine();
@@ -51,12 +53,23 @@ internal class Program
             {
                 Content = "Test",
                 Text = "TextQuestion"
-            },
+            } /*,
             new VariantsQuestion
             {
                 Content = ["Variant1", "Variant2"],
                 Text = "VariantsQuestion"
-            });
+            }*/
+        );
+
+        context.SaveChanges();
+
+        AnswerFactory answerFactory = new();
+
+        foreach (Question question in context.Questions)
+        {
+            Answer entity = answerFactory.Create(question, "1");
+            context.Answers.Add(entity);
+        }
 
         context.SaveChanges();
 
@@ -64,28 +77,97 @@ internal class Program
     }
 }
 
-public abstract class Identity
+internal abstract class Identity
 {
     public Guid Id { get; set; }
 }
 
+internal class AnamnesisAnswer : Identity
+{
+    public required Guid QuestionId { get; set; }
+    public virtual Question Question { get; set; } = null!;
+
+    public virtual IList<Answer> Answers { get; set; } = null!;
+}
+
+internal class Answer : Identity
+{
+    public Answer(string text, Question question)
+    {
+        Text = text;
+        Question = question;
+    }
+
+    public Answer()
+    {
+    }
+
+    public string Text { get; private set; }
+
+    public Question Question { get; private set; }
+}
+
+internal class ParameterAnswer<T> : Answer
+{
+    public ParameterAnswer()
+    {
+    }
+
+    public ParameterAnswer(T content, Question question) : base(content.ToString(), question)
+    {
+        Content = content;
+    }
+
+    public T Content { get; private set; }
+}
+
+internal enum ContentType
+{
+    None,
+    Text,
+
+    //  Selection,
+    Numeric
+    //  Date
+}
+
+internal class AnswerFactory
+{
+    public Answer Create(Question question, object content)
+    {
+        ContentType type = question.AnswerType;
+
+        return type switch
+        {
+            ContentType.None => new Answer(content as string ?? throw new InvalidOperationException(), question),
+            ContentType.Text => new ParameterAnswer<string>(content as string ?? throw new InvalidOperationException(), question),
+            ContentType.Numeric => new ParameterAnswer<int>(Convert.ToInt32(content), question),
+            var _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+}
+
 internal class Question : Identity
 {
+    public virtual ContentType AnswerType { get; set; }
     public virtual required string Text { get; set; }
 }
 
-internal class NumericQuestion : Question, IContentHaving<int>
+internal class NumericQuestion : Question
 {
-    
     public required int Content { get; set; }
+
+    public override ContentType AnswerType { get; set; } = ContentType.Numeric;
 }
 
-internal class TextQuestion : Question, IContentHaving<string>
+internal class TextQuestion : Question
 {
     public required string Content { get; set; }
+
+    public override ContentType AnswerType { get; set; } = ContentType.Text;
 }
 
-internal class VariantsQuestion : Question, IContentHaving<IList<string>>
+internal class VariantsQuestion : Question
 {
     public required IList<string> Content { get; set; }
 }
