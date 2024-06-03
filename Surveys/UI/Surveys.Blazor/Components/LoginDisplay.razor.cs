8b7451a04de54c89810a933f103b69f22d94dd26
Савелий Вотинцев <votintcev1@mail.ru>
2024-06-03 13:46:05 +0300

    Clean up login component.

---
+++ Surveys/UI/Surveys.Blazor/Components/LoginDisplay.razor.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace Surveys.Blazor.Components;
+
+public partial class LoginDisplay
+{
+    [Inject] private NavigationManager Navigation { get; set; } = null!;
+
+    public void BeginLogOut()
+    {
+        Navigation.NavigateToLogout("authentication/logout");
+    }
+
+    public void BeginLogIn()
+    {
+        Navigation.NavigateToLogout("authentication/login");
+    }
+}
