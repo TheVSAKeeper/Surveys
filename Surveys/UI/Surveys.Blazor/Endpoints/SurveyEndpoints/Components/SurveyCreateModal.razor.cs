417f3fb80deaed724d133c2eb67d9eb15b25a403
Савелий Вотинцев <votintcev1@mail.ru>
2024-06-07 00:26:34 +0300

    Added default survey.

--- Surveys/UI/Surveys.Blazor/Endpoints/SurveyEndpoints/Components/SurveyCreateModal.razor.cs
+++ Surveys/UI/Surveys.Blazor/Endpoints/SurveyEndpoints/Components/SurveyCreateModal.razor.cs
@@ -3,6 +3,9 @@ using Blazorise;
 using Microsoft.AspNetCore.Components;
 using Surveys.Blazor.Domain;
 using Surveys.Blazor.Services;
+using Surveys.Domain;
+using Surveys.Web.Application.Messaging.AnamnesisMessages;
+using Surveys.Web.Application.Messaging.AnamnesisTemplateMessages.ViewModels;
 using Surveys.Web.Application.Messaging.PatientMessages.ViewModels;
 using Surveys.Web.Application.Messaging.SurveyMessages.ViewModels;
 
@@ -10,6 +13,7 @@ namespace Surveys.Blazor.Endpoints.SurveyEndpoints.Components;
 
 public partial class SurveyCreateModal
 {
+    private bool _isDefaultSurvey;
     private Modal _modal = null!;
     private SurveyCreateViewModel? _createViewModel;
     private Validations? _fluentValidations;
@@ -28,7 +32,12 @@ public partial class SurveyCreateModal
         Operation<SurveyViewModel>? survey = await response.Content.ReadFromJsonAsync<Operation<SurveyViewModel>>();
 
         if (survey is { Ok: true } && _createViewModel is not null)
+        {
             _createViewModel.Id = survey.Result.Id;
+
+            if (_isDefaultSurvey)
+                await FillBaseSurvey(_createViewModel.Id);
+        }
 
         if (response.IsSuccessStatusCode)
             await MessageService.Success("Опрос успешно создан!", "Создание опроса");
@@ -37,6 +46,29 @@ public partial class SurveyCreateModal
 
         await OnSubmitComplaint.InvokeAsync(_createViewModel);
         await _modal.Hide();
+    }
+
+    private async Task FillBaseSurvey(Guid id)
+    {
+        Operation<PagedListResult<AnamnesisTemplateViewModel>>? response = await Client
+            .GetFromJsonAsync<PagedListResult<AnamnesisTemplateViewModel>>($"anamnesis-template/paged/{0}?pageSize={999}");
+
+        if (response == null || response.Ok == false)
+            return;
+
+        List<AnamnesisTemplateViewModel> templates = [..response.Result.Items.ToList()];
+
+        if (_createViewModel is { Patient: not null } && _createViewModel.Patient.Gender != Gender.Female)
+            templates = templates.Where(model => string.Equals(model.Title, "Гинекологический анамнез", StringComparison.InvariantCultureIgnoreCase) == false).ToList();
+
+        foreach (AnamnesisTemplateViewModel template in templates)
+        {
+            await Client.PostAsync("anamneses", new AnamnesisCreateViewModel
+            {
+                SurveyId = id,
+                AnamnesisTemplateId = template.Id
+            });
+        }
     }
 
     public void Show(PatientViewModel patient)
