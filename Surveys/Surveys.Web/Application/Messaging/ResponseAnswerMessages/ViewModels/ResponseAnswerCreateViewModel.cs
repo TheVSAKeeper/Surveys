96603081bf710cd6ef61f8214ab0fb0d9dbbce4c
Савелий Вотинцев <votintcev1@mail.ru>
2024-06-06 19:21:42 +0300

    Added required modifier.

--- Surveys/Surveys.Web/Application/Messaging/ResponseAnswerMessages/ViewModels/ResponseAnswerCreateViewModel.cs
+++ Surveys/Surveys.Web/Application/Messaging/ResponseAnswerMessages/ViewModels/ResponseAnswerCreateViewModel.cs
@@ -3,6 +3,6 @@ namespace Surveys.Web.Application.Messaging.ResponseAnswerMessages.ViewModels;
 public class ResponseAnswerCreateViewModel
 {
     public required string Value { get; set; }
-    public Guid ResponseId { get; set; }
-    public Guid QuestionId { get; set; }
+    public required Guid ResponseId { get; set; }
+    public required Guid QuestionId { get; set; }
 }
--- Surveys/Surveys.Web/Application/Messaging/ResponseAnswerMessages/ViewModels/ResponseAnswerUpdateViewModel.cs
+++ Surveys/Surveys.Web/Application/Messaging/ResponseAnswerMessages/ViewModels/ResponseAnswerUpdateViewModel.cs
@@ -2,6 +2,6 @@ namespace Surveys.Web.Application.Messaging.ResponseAnswerMessages.ViewModels;
 
 public class ResponseAnswerUpdateViewModel
 {
-    public Guid Id { get; set; }
+    public required Guid Id { get; set; }
     public required string Value { get; set; }
 }
